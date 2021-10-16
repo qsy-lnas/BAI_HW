aa2289cb7c625ddde235ff9cb4509d1c58b5c3da
qsy-lnas <lnasqsy@qq.com>
2021-10-16 21:09:24 +0800

    PJ1_dfs_timeout

--- project1/project1.py
+++ project1/project1.py
@@ -12,9 +12,9 @@ class single_game(object):
         self.acards = self.card_in_np()
         #最佳策略与最短步数
         self.strategy = {}
-        self.steps = 0
-
-        #print(self.cards)
+        self.steps = 15
+        self.steps = self.dfs()
+        print(self.steps)
     
     def card_in_np(self):
         acards = np.zeros(15)
@@ -30,10 +30,135 @@ class single_game(object):
         k = 0
         for i in range(values.two.value):#小王以下均可顺子
             if self.acards[i] == 0: k = 0 # 顺子中断
+            else: # 通过回溯遍历不同长度的顺子
+                k += 1
+                if k >= 5:
+                    # 出牌
+                    for j in range(i, i - k): self.acards[j] -= 1
+                    self.dfs(x + 1)
+                    #回溯
+                    for j in range(i, i - k): self.acards[j] += 1
+        #间隔单顺子
+        k = 0
+        for q in range(2):
+            for i in range(q, values.two.value, 2):
+                if self.acards[i] == 0: k = 0 # 顺子中断
+                else:
+                    k += 1
+                    if k >= 5:
+                        for j in range(i, i - 2 * k, 2): self.acards[j] -= 1
+                        self.dfs(x + 1)
+                        for j in range(i, i - 2 * k, 2): self.acards[j] += 1
+        #双顺子
+        k = 0
+        for i in range(values.two.value):
+            if self.acards[i] < 2: k = 0
             else:
                 k += 1
-                if k > 5:
-                    for 
+                if k >= 3:
+                    for j in range(i, i - k): self.acards[j] -= 2
+                    self.dfs(x + 2)
+                    for j in range(i, i - k): self.acards[j] += 2
+        #间隔双顺子
+        k = 0
+        for q in range(2):
+            for i in range(q, values.two.value, 2):
+                if self.acards[i] < 2: k = 0
+                else:
+                    k += 1
+                    if k >= 3:
+                        for j in range(i, i - 2 * k, 2): self.acards[j] -= 2
+                        self.dfs(x + 1)
+                        for j in range(i, i - 2 * k, 2): self.acards[j] += 2
+        #三顺子
+        for i in range(values.two.value):
+            if self.acards[i] < 3: k = 0;
+            else:
+                k += 1
+                if k >= 2:
+                    for j in range(i, i - k): self.acards[j] -= 3
+                    self.dfs(x + 1)
+                    for j in range(i, i - k): self.acards[j] -= 3
+        #间隔三顺子
+        for q in range(2):
+            for i in range(q, values.two.value, 2):
+                if self.acards[i] < 3: k = 0;
+                else:
+                    k += 1
+                    if k >= 2:
+                        for j in range(i, i - 2 * k, 2): self.acards[j] -= 3
+                        self.dfs(x + 1)
+                        for j in range(i, i - 2 * k, 2): self.acards[j] -= 3
+        #带牌
+        for i in range(values.sjoker.value):#枚举小王及以下的牌可以作为带牌的主体
+            if self.acards[i] <= 3:
+                if self.acards[i] <= 2: continue # 排除两张以下
+                self.acards[i] -= 3 # 减去带牌
+                for j in range(values.ljoker.value + 1): # 带单张
+                    if self.acards[j] == 0: continue # 无牌(无需考虑相同)
+                    self.acards[j] -= 1 # 出牌
+                    self.dfs(x + 1)
+                    self.acards[j] += 3 # 回溯
+                for j in range(values.sjoker.value): # 带一对
+                    if self.acards[j] <= 1: continue
+                    self.acards[j] -= 2
+                    self.dfs(x + 1)
+                    self.acards[j] += 2
+                self.acards[i] += 3 # 回溯
+            else: # 四张也可以选择三带
+                self.acards[i] -= 3 # 先3带
+                for j in range(values.ljoker.value + 1): # 带单张
+                    if (self.acards[j] == 0) or (j == i): continue # 无牌或相同
+                    self.acards[j] -= 1 # 出牌
+                    self.dfs(x + 1)
+                    self.acards[j] += 3 # 回溯
+                for j in range(values.sjoker.value): # 带一对
+                    if self.acards[j] <= 1: continue
+                    self.acards[j] -= 2 #出对子
+                    self.dfs(x + 1)
+                    self.acards[j] += 2 # 回溯
+                self.acards[i] += 3 # 回溯
+
+                self.acards[i] -= 4 # 四张带(包含炸弹)
+                for j in range(values.ljoker.value + 1): # 两个单张
+                    if self.acards[j] == 0: continue
+                    self.acards[j] -= 1 # 出第一张牌
+                    for m in range(values.ljoker.value + 1): # 另一个单张
+                        if (self.acards[m] == 0) or (j == m): continue
+                        self.acards[m] -= 1 #出第二张牌
+                        self.dfs(x + 1)
+                        self.acards[m] += 1 # 回溯
+                    self.acards[j] += 1 # 回溯
+                for j in range(values.sjoker.value): # 带两个对子
+                    if self.acards[j] <= 1: continue
+                    self.acards[j] -= 2
+                    for m in range(values.sjoker.value):
+                        if (self.acards[m] <= 1) or (j == m): continue
+                        self.acards[m] -= 2
+                        self.dfs(x + 1)
+                        self.acards[m] += 2
+                    self.acards[j] += 2
+                self.acards[i] += 4
+        for i in range(values.ljoker.value + 1):
+            if self.acards[i]: x += 1
+        self.steps = min(self.steps, x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                 
     
 
