5917e186e41d04f806225fa9f56c548aff23c52f
qsy-lnas <lnasqsy@qq.com>
2021-11-21 21:42:17 +0800

    PJ2 dataset add tensor

--- project2/EmDataset.py
+++ project2/EmDataset.py
@@ -5,8 +5,9 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 
 class EmDataset(Dataset):
-    def __init__(self, split = "train"):
+    def __init__(self, split = "train", device = 0):
         super().__init__()
+        self.device = device
         data_pd = pd.read_csv('project2/figure.csv')
         data = data_pd.to_numpy()
         labels = data[:, 0]
@@ -18,15 +19,15 @@ class EmDataset(Dataset):
             index = usage == "Test"
         self.label = labels[index]
         self.feature = features[index]
-
+        self.feature = self.feature.reshape(self.label.shape[0], 1, 48, 48)
         #print(self.feature.shape)
 
     def __len__(self):
         return self.label.shape[0]
 
     def __get_item__(self, i):
-
-        return self.feature[i], self.label[i]
+        face_tensor = torch.from_numpy(self.feature[i]).cuda(self.device)
+        return face_tensor, self.label[i]
         
 
 
